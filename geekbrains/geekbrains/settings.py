8b62633129eba5ea3f3f84edc6f658f0a1fa39da
Mikhail Kazachkin <mikhail.kazachkin@yandex.ru>
2024-04-14 21:03:00 +0300

    Preparing for online

--- geekbrains/geekbrains/settings.py
+++ geekbrains/geekbrains/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&q4nl710y8s-d78t7x7nqc)3-(wniz%+ox(xz@af=jdb9gsp(j'
+#SECRET_KEY = 'django-insecure-&q4nl710y8s-d78t7x7nqc)3-(wniz%+ox(xz@af=jdb9gsp(j'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '192.168.1.254']
+ALLOWED_HOSTS = ['127.0.0.1', 'mkazachkin.pythonanywhere.com']
 
 
 # Application definition
@@ -78,8 +81,19 @@ WSGI_APPLICATION = 'geekbrains.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mkazachkin$default',
+        'USER': 'mkazachkin',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'mkazachkin.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES';",
+            'charset': 'utf8mb4',
+        },
+
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -119,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static",
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -177,3 +192,4 @@ LOGGING = {
         }
     },
 }
+
